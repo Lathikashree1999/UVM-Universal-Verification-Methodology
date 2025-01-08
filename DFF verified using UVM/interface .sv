e8982e314b6c0670ec55342f24e9305223a22dfb
Lathikashree1999 <126261663+Lathikashree1999@users.noreply.github.com>
2025-01-08 13:23:29 +0530

    Create interface .sv

---
+++ DFF verified using UVM/interface .sv
@@ -0,0 +1,10 @@
+interface inter(input logic clk);//clock is generating and comming from (testbench.sv) top module so it is input for interface
+  //interface has all dut signals
+  //Declare with logic type
+  //logic clk;
+  
+  logic rst;
+  logic d;
+  logic q;
+  
+endinterface
